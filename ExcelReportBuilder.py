c55c1d9ba0fdb02afe9be74b6a5d1d2a7ef190d2
Vladimir Bondarenko <VBondarenko@armi-marketing.com>
2024-05-30 11:26:21 +0300

    added table headers

--- ExcelReportBuilder.py
+++ ExcelReportBuilder.py
@@ -1,14 +1,15 @@
+import io
+
 from openpyxl import Workbook
 from openpyxl.drawing.image import Image
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.formatting.rule import ColorScaleRule
+from openpyxl.styles import Font
 
-import io
 
 import pandas as pd
 
 class ExcelReportBuilder:
-
 
     def __init__(self, file_name): 
         self.images_ = []
@@ -18,8 +19,23 @@ class ExcelReportBuilder:
     def AddImage(self, stream: io.BytesIO, page_name: str, position: str="A1"):
         self.images_.append({'page': page_name, 'image': Image(stream), 'position': position})
     
-    def AddTable(self, table: pd.DataFrame, page_name: str, drop_index=False, conditional_formatting=False):
-        self.tables_.append({'page': page_name, 'table': table, 'index': not(drop_index), 'conditional_formatting': conditional_formatting})
+    def AddTable(self, 
+                 table: pd.DataFrame, 
+                 page_name: str, 
+                 title: None, 
+                 description=None, 
+                 drop_index=False, 
+                 conditional_formatting=False):
+        self.tables_.append(
+            {
+                'title': title, 
+                'description': description,
+                'page': page_name,
+                'table': table, 
+                'index': not(drop_index), 
+                'conditional_formatting': conditional_formatting
+            }
+        )
 
     @staticmethod
     def __GetSheetPtr(wb: Workbook, name: str):
@@ -32,11 +48,26 @@ class ExcelReportBuilder:
     def SaveToFile(self):
         wb = Workbook()
         
+        #Writing tables
         for tab in self.tables_:
             ws = ExcelReportBuilder.__GetSheetPtr(wb, tab['page'])
-            start_row = ws.max_row + 1
+            start_row = 1 if ws.max_row == 1 else ws.max_row + 2
+
+            if tab['title']: 
+                ws.cell(start_row, 1, tab['title']).font = Font(bold=True)
+                start_row += 1
+
+            if tab['description']: 
+                ws.cell(start_row, 1, tab['description'])
+                start_row += 1
+
             for r in dataframe_to_rows(tab['table'], index=tab['index'], header=True):
                 ws.append(r)
+
+            for r in range(start_row, ws.max_row+1):
+                for cell in ws[r]:
+                    cell.style = 'Pandas'
+
             if tab['conditional_formatting']:
                 formatting_range = '{}{}:{}'.format(
                     ('A' if not tab['index'] else 'B'), 
@@ -51,6 +82,7 @@ class ExcelReportBuilder:
                     ))
             ws.append([])
 
+        # Writing images
         for img in self.images_:
             ws = ExcelReportBuilder.__GetSheetPtr(wb, img['page'])
             ws.add_image(img['image'], img['position'])
